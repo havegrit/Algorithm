9b3cf56d5127c994bba08f0ea02a2ffd3b591c6b
havegrit <79460763+havegrit@users.noreply.github.com>
2023-03-13 18:58:45 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.03 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] solution(int[] numbers, String direction) {
+        int[] answer = new int[numbers.length];
+        int i;
+        if (direction.equals("right")) {
+            i = 1;
+            for (int bit : numbers) {
+                if (i == numbers.length) {
+                    answer[0] = bit;
+                }else{
+                    answer[i++] = bit;
+                }
+            }
+        } else {
+            i = numbers.length;
+            for (int bit : numbers) {
+                if (i == numbers.length) {
+                    answer[numbers.length-1] = bit;
+                    i = 0;
+                }else{
+                    answer[i++] = bit;
+                }
+            }
+        }
+        return answer;
+    }
+}
