e6c2fca01cdc89f180db4beac82c4d6a19f85e18
havegrit <79460763+havegrit@users.noreply.github.com>
2023-03-20 23:32:29 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.02 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.java
+++ 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.java
@@ -1,11 +1,9 @@
-import java.util.Arrays;
 class Solution {
     public int[][] solution(int[] num_list, int n) {
         int[][] answer = new int[num_list.length/n][n];
-        String[] bitsOfNumList = Arrays.toString(num_list).replace("[","").replace("]","").split(", ");
         for (int i = 0; i < num_list.length/n; i++) {
             for (int j = 0; j < n; j++) {
-                answer[i][j] = Integer.parseInt(bitsOfNumList[(n*i)+j]);
+                answer[i][j] = num_list[(i * n) + j];
             }
         }
         return answer;
