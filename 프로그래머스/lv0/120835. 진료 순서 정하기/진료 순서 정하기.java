c29f3302d0b2076fed7aad0cf159070e6b26bafc
havegrit <79460763+havegrit@users.noreply.github.com>
2023-03-28 10:05:13 +0900

    [level 0] Title: 진료 순서 정하기, Time: 2.13 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+class Solution {
+    public int[] solution(int[] emergency) {
+        int[] ordered = Arrays.stream(emergency).sorted().toArray();
+        for (int i = 0; i < ordered.length / 2; i++) {
+            int temp = ordered[i];
+            ordered[i] = ordered[ordered.length - 1 - i];
+            ordered[ordered.length - 1 - i] = temp;
+        }
+        for (int i = 0; i < emergency.length; i++) {
+            for (int j = 0; j < ordered.length; j++) {
+                if(emergency[i] == ordered[j]) {
+                    emergency[i] = j + 1;
+                    break;
+                }
+            }
+        }
+        return emergency;
+    }
+}
