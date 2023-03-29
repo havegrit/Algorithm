ece7e243f7596e1821217f6b53e13dc62226becf
havegrit <79460763+havegrit@users.noreply.github.com>
2023-03-30 02:04:39 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.07 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        StringBuilder num = new StringBuilder();
+        for (int i = 0; i < my_string.length(); i++) {
+            if (Character.isDigit(my_string.charAt(i))) {
+                num.append(my_string.charAt(i));
+            } else {
+                if(num.length() > 0) {
+                    answer += Integer.parseInt(num.toString());
+                    num.setLength(0);
+                }
+            }
+        }
+        if(num.length() > 0) answer+=Integer.parseInt(num.toString());
+        return answer;
+    }
+}
