a9b535333ba771a308000e3dff51a3d759f3ccbf
havegrit <79460763+havegrit@users.noreply.github.com>
2023-04-19 21:50:09 +0900

    [level 2] Title: 하노이의 탑, Time: 2.83 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/lv2/12946. 하노이의 탑/하노이의 탑.java
+++ 프로그래머스/lv2/12946. 하노이의 탑/하노이의 탑.java
@@ -1,36 +1,6 @@
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
     public int[][] solution(int n) {
-        return new Hanoi(1,3,n).toArray();
-    }
-}
-
-class Hanoi {
-    private final List<int[]> paths = new ArrayList<>();
-
-    public Hanoi(int from, int to, int n) {
-        calculate(from, to, n);
-    }
-
-    public void calculate(int from, int to, int n) {
-        int empty = 6 - (from + to);
-
-        if (n == 1) {
-            addPath(from, to);
-            return;
-        }
-        calculate(from, empty, n - 1);
-        calculate(from, to, 1);
-        calculate(empty, to, n - 1);
-    }
-
-    private void addPath(int from, int to) {
-        paths.add(new int[]{from, to});
-    }
-
-    public int[][] toArray() {
-        return paths.toArray(int[][]::new);
+        int[][] answer = {};
+        return answer;
     }
 }
