4d81f7b743a27a0ddf408b924797e3a2eee2c865
havegrit <79460763+havegrit@users.noreply.github.com>
2023-08-04 18:59:46 +0900

    [Bronze V] Title: A+B - 8, Time: 308 ms, Memory: 18584 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        for (int i = 1; i <= t; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.printf("Case #%d: ", i);
+            
+            System.out.printf("%d + %d = %d\n", a, b, a+b);     
+        }
+    }
+}
