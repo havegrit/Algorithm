ae28579a856ca5b51c31cb1d36892b507c4e7754
havegrit <79460763+havegrit@users.noreply.github.com>
2023-08-11 22:20:16 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 140 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -1,12 +1,13 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        while(sc.hasNext()) {
-            String word = sc.nextLine();
-            System.out.println(word);
+        String str = "";
+
+        while ((str = br.readLine()) != null) {
+            System.out.println(str);
         }
     }
 }
