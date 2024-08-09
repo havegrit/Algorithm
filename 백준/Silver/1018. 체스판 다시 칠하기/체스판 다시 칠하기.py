d040d44d258d44c4f18e7364826340a7ad649790
havegrit <79460763+havegrit@users.noreply.github.com>
2024-08-09 20:54:49 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 60 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -1,53 +1,35 @@
-userInput = input().split(' ')
-N = int(userInput[0])
-M = int(userInput[1])
+def count_repaint(board_segment, reference_board):
+    repaint_count = 0
+    for row, ref_row in zip(board_segment, reference_board):
+        for cell, ref_cell in zip(row, ref_row):
+            if cell != ref_cell:
+                repaint_count += 1
+    return repaint_count
 
-answer = 65
-board = [0]* N
-W = 'W'
-B = 'B'
-whiteStarts = [W, B, W, B, W, B, W, B]
-blackStarts = [B, W, B, W, B, W, B, W]
-whiteStartChessBoard = [
-  whiteStarts,
-  blackStarts,
-  whiteStarts,
-  blackStarts,
-  whiteStarts,
-  blackStarts,
-  whiteStarts,
-  blackStarts
-]
-blackStartChessBoard = [
-  blackStarts,
-  whiteStarts,
-  blackStarts,
-  whiteStarts,
-  blackStarts,
-  whiteStarts,
-  blackStarts,
-  whiteStarts
-]
+def min_repaint_chess_board(N, M, board):
+    answer = float('inf')
+    board_size = 8
 
-for i in range(0, N):
-  board[i] = input()
+    white_starts = ['W', 'B'] * 4
+    black_starts = ['B', 'W'] * 4
 
-for i in range(0, N-7):
-  for j in range(0, M-7):
-    cnt = 0
-    cntRepaint = 0
-    whiteStartRepaintCount = 0
-    blackStartRepaintCount = 0
-    for partOfBoard in board[i:i+8]:
-      for a, b in zip(partOfBoard[j:j+8], whiteStartChessBoard[cnt]):
-        if a != b:
-          whiteStartRepaintCount += 1
-      for a, b in zip(partOfBoard[j:j+8], blackStartChessBoard[cnt]):
-        if a != b:
-          blackStartRepaintCount += 1
-      cnt += 1
-    repaintCount = whiteStartRepaintCount if whiteStartRepaintCount < blackStartRepaintCount else blackStartRepaintCount
-    if repaintCount < answer:
-      answer = repaintCount
+    white_start_chess_board = [white_starts if i % 2 == 0 else black_starts for i in range(board_size)]
+    black_start_chess_board = [black_starts if i % 2 == 0 else white_starts for i in range(board_size)]
 
-print(answer)
+    for i in range(N - board_size + 1):
+        for j in range(M - board_size + 1):
+            board_segment = [row[j:j + board_size] for row in board[i:i + board_size]]
+
+            white_repaint = count_repaint(board_segment, white_start_chess_board)
+            black_repaint = count_repaint(board_segment, black_start_chess_board)
+
+            answer = min(answer, white_repaint, black_repaint)
+
+    return answer
+
+user_input = input().split()
+N = int(user_input[0])
+M = int(user_input[1])
+
+board = [input() for _ in range(N)]
+print(min_repaint_chess_board(N, M, board))
