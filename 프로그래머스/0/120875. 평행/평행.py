e90090383061778f12eade6e62b38d616f94cb85
havegrit <79460763+havegrit@users.noreply.github.com>
2024-09-10 21:31:30 +0900

    [level 0] Title: 평행, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120875. 평행/평행.py
+++ 프로그래머스/0/120875. 평행/평행.py
@@ -4,10 +4,8 @@ def solution(dots):
     return 0
 
 def isParellel(dot1, dot2, dot3, dot4):
-    w = abs(dot1[0] - dot2[0])
-    h = abs(dot1[1] - dot2[1])
-    if (abs(dot3[0] - dot4[0]) == w and abs(dot3[1] - dot4[1]) == h):
-        return 1
-    if abs(dot3[0] - dot4[0]) % w == 0 and abs(dot3[1] - dot4[1]) % h == 0 and abs(dot3[0] - dot4[0]) // w == abs(dot3[1] - dot4[1]) // h:
-        return 1
-    return 0
+    dx1 = dot1[0] - dot2[0]
+    dy1 = dot1[1] - dot2[1]
+    dx2 = dot3[0] - dot4[0]
+    dy2 = dot3[1] - dot4[1]
+    return dy1*dx2 == dy2*dx1
