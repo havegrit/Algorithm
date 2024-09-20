78669fcfc11c410fd7dd37bc44a59b297321e27f
havegrit <79460763+havegrit@users.noreply.github.com>
2024-09-20 14:24:22 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.40 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.py
@@ -0,0 +1,24 @@
+def solution(num, total):
+    answer = []
+    start = 0
+    end = num
+    
+    while True:
+        answer.clear()
+        summ = 0
+            
+        for i in range(start, end):
+            answer.append(i)
+            summ += i
+        
+        if summ == total:
+            break
+            
+        if total < summ:
+            start -= 1
+            end -= 1
+        else:
+            start += 1
+            end += 1
+        
+    return answer
