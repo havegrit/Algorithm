b5312bc43c3a032ca63169e5688840bfadd3e49d
havegrit <79460763+havegrit@users.noreply.github.com>
2024-09-20 12:19:50 +0900

    [level 0] Title: 종이 자르기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120922. 종이 자르기/종이 자르기.py
+++ 프로그래머스/0/120922. 종이 자르기/종이 자르기.py
@@ -1,11 +1,2 @@
 def solution(M, N):
-    answer = 0
-    
-    if M == 1 and N == 1:
-        return 0
-    elif M != 1:
-        answer += solution(M // 2, N) + solution(M - M // 2, N) + 1
-    elif N != 1:
-        answer += solution(M, N // 2) + solution(M, N - N // 2) + 1
-    
-    return answer
+    return (M * N) - 1
