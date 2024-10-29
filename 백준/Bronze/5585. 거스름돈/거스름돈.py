c735e0d2584fdbfc91a17dabe3ebd021fac3ba37
havegrit <79460763+havegrit@users.noreply.github.com>
2024-10-29 22:02:03 +0900

    [Bronze II] Title: 거스름돈, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/5585. 거스름돈/거스름돈.py
+++ 백준/Bronze/5585. 거스름돈/거스름돈.py
@@ -2,24 +2,10 @@ change = 1000 - int(input())
 
 answer = 0
 
-while change > 0:
-    if change >= 500:
-        change -= 500
-        answer += 1
-    elif change >= 100:
-        change -= 100
-        answer += 1
-    elif change >= 50:
-        change -= 50
-        answer += 1
-    elif change >= 10:
-        change -= 10
-        answer += 1
-    elif change >= 5:
-        change -= 5
-        answer += 1
-    elif change >= 1:
-        change -= 1
-        answer += 1
+coins = [500, 100, 50, 10, 5, 1]
+
+for coin in coins:
+    answer += change // coin
+    change = change % coin
 
 print(answer)
