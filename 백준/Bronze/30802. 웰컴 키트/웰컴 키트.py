904deafb8dbef579f7d1f72ba3d417b4b5af031d
havegrit <79460763+havegrit@users.noreply.github.com>
2024-11-06 22:59:41 +0900

    [Bronze III] Title: 웰컴 키트, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/30802. 웰컴 키트/웰컴 키트.py
@@ -0,0 +1,17 @@
+n = int(input())
+sizes = list(map(int, input().split()))[:6] 
+t, p = map(int, input().split())
+t_order = 0
+
+p1 = n // p
+p2 = n % p
+
+for size in sizes:
+    t_order += size // t
+    size = size % t
+    if size % t > 0:
+        t_order += 1
+        
+print(t_order)
+print(f"{p1} {p2}")
+        
