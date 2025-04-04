6984737d5e1d54d66d9e5a0a12a77b3a3fb15e25
havegrit <79460763+havegrit@users.noreply.github.com>
2025-04-04 21:21:10 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1796 ms, Memory: 342392 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+class Main {
+    private static int MAX_VALUE = 10001;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] given = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            given[i] = Integer.parseInt(br.readLine());
+        }
+
+        countingSort(given);
+    }
+
+    private static void countingSort(int[] list) throws IOException {
+        int[] countArray = new int[MAX_VALUE];
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for (int i = 0; i < list.length; i++) {
+            countArray[list[i]]++;
+        }
+
+        for (int i = 0; i < MAX_VALUE; i++) {
+            if (countArray[i] > 0) {
+                for (int j = 0; j < countArray[i]; j++) {
+                    bw.write(i + "\n");
+                }
+            }
+        }
+        bw.flush();
+        bw.close();
+    }
+}
